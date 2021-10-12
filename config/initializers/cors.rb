987316e8f41fba4f8791813725bb1078f5c491bc
takashi <mr.takamuu@gmail.com>
2021-10-12 15:50:04 +0900

    Added CORS and host settings

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '127.0.0.1:5500'
+    origins 'https://takamuu.github.io'
 
     resource '*',
       headers: :any,
